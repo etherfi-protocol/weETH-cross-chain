ebd18962e96eeaee20481b25717986d30f8c1c4b
jtfirek <firekjt@gmail.com>
2024-10-14 17:52:20 -0500

    fix

--- contracts/MintableOFTUpgradeable.sol
+++ contracts/MintableOFTUpgradeable.sol
@@ -2,21 +2,18 @@
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
-
 import {OFTUpgradeable} from "@layerzerolabs/lz-evm-oapp-v2/contracts-upgradeable/oft/OFTUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
 import {IMintableERC20} from "../interfaces/IMintableERC20.sol";
-import {PairwiseRateLimiter} from "./PairwiseRateLimiter.sol";
+import { RateLimiter } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/utils/RateLimiter.sol";
 
 /**
- * @title Etherfi mintable upgradeable OFT token
- * @dev Extends MintableOFTUpgradeable with pausing and rate limiting functionality
+ * @title Mintable OFT
+ * @dev An OFT token that can be minted by a minter.
  */
-contract EtherfiOFTUpgradeable is OFTUpgradeable, AccessControlUpgradeable, PausableUpgradeable, PairwiseRateLimiter, IMintableERC20 {
+contract MintableOFTUpgradeable is RateLimiter, OFTUpgradeable, AccessControlUpgradeable, IMintableERC20 {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant UNPAUSER_ROLE = keccak256("UNPAUSER_ROLE");
 
     /**
      * @dev Constructor for MintableOFT
@@ -43,23 +40,13 @@ contract EtherfiOFTUpgradeable is OFTUpgradeable, AccessControlUpgradeable, Paus
         uint256 _amountLD,
         uint256 _minAmountLD,
         uint32 _dstEid
-    ) internal virtual override whenNotPaused() returns (uint256 amountSentLD, uint256 amountReceivedLD) {
-        _checkAndUpdateOutboundRateLimit(_dstEid, _amountLD);
+    ) internal virtual override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        _checkAndUpdateRateLimit(_dstEid, _amountLD);
         return super._debit(_amountLD, _minAmountLD, _dstEid);
     }
 
-    function _credit(
-        address _to,
-        uint256 _amountLD,
-        uint32 _srcEid
-    ) internal virtual override whenNotPaused() returns (uint256 amountReceivedLD) {
-        _checkAndUpdateInboundRateLimit(_srcEid, _amountLD);
-        return super._credit(_to, _amountLD, 0);
-    }
-
     /**
-     * @notice Mint function that can only be called by a minter
-     * @dev Used by the SyncPool contract in the native minting flow
+     * @dev Mint function that can only be called by a minter
      * @param _account The account to mint the tokens to
      * @param _amount The amount of tokens to mint
      */
@@ -67,42 +54,17 @@ contract EtherfiOFTUpgradeable is OFTUpgradeable, AccessControlUpgradeable, Paus
         _mint(_account, _amount);
     }
     
-    function setOutboundRateLimits(RateLimitConfig[] calldata _rateLimitConfigs) external onlyOwner() {
-        _setOutboundRateLimits(_rateLimitConfigs);
+    function setRateLimits(RateLimitConfig[] calldata _rateLimitConfigs) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setRateLimits(_rateLimitConfigs);
     }
 
-    function setInboundRateLimits(RateLimitConfig[] calldata _rateLimitConfigs) external onlyOwner() {
-        _setInboundRateLimits(_rateLimitConfigs);
-    }
-
-
-   function pauseBridge() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpauseBridge() external onlyRole(UNPAUSER_ROLE) {
-        _unpause();
-    }
-
-    /**
-     * @dev Overrides the role admin logic from AccessControlUpgradeable to restrict granting roles to the owner
-     */
-    function grantRole(bytes32 role, address account) public override onlyOwner() {
-        _grantRole(role, account);
-    }
-
-    /**
-     * @dev Overrides the role admin logic from AccessControlUpgradeable to restrict revoking roles to the owner
-     */
-    function revokeRole(bytes32 role, address account) public override onlyOwner() {
-        _revokeRole(role, account);
-    }
-
-    function updateTokenSymbol(string memory name_, string memory symbol_) external onlyOwner() {
+    function updateTokenSymbol(string memory name_, string memory symbol_) external onlyRole(DEFAULT_ADMIN_ROLE) {
         ERC20Storage storage $ = getERC20Storage();
         $._name = name_;
         $._symbol = symbol_;
     }
+
+    // // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.ERC20")) - 1)) & ~bytes32(uint256(0xff))
 
     function getERC20Storage() internal pure returns (ERC20Storage storage $) {
         bytes32 ERC20StorageLocation = 0x52c63247e1f47db19d5ce0460030c497f067ca4cebf71ba98eeadabe20bace00;
