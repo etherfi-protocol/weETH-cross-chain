13638c39a14bbb9b8c9ed836f4fb44d0e232d78b
jtfirek <firekjt@gmail.com>
2024-10-21 11:28:20 -0500

    adding more mocking

--- scripts/DeployTestnet/deployMainnetContracts.s.sol
+++ scripts/DeployTestnet/deployMainnetContracts.s.sol
@@ -9,23 +9,27 @@ import "../../contracts/EtherFiTimelock.sol";
 import "../../utils/L2Constants.sol";
 
 contract mockEETH is ERC20 {
-    constructor() ERC20("EtherFi ETH", "eETH") {
-        _mint(msg.sender, 1000000000000000000000000000);
-    }
+    constructor() ERC20("EtherFi ETH", "eETH") {}
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
 }
 
 contract mockWEETH is ERC20 {
-
-    constructor() ERC20("Wrapped EtherFi ETH", "weETH") {
-        _mint(msg.sender, 1000000000000000000000000000);
+    mockEETH public eeth;
+    constructor(address _eeth) ERC20("Wrapped EtherFi ETH", "weETH") {
+        eeth = mockEETH(_eeth);
     }
 
-    
-
+    function wrap(uint256 amount) public {
+        _mint(msg.sender, amount);
+        eeth.burn(msg.sender, amount);
+    }
 }
 
 contract mockLiquifier {
@@ -37,13 +41,17 @@ contract mockLiquifier {
         weeth = mockWEETH(_weeth);
     }
 
+    // mints our mock eeth to the recipient
     function depositWithERC20(address token, uint256 amount, address referral) public {
-        
+        if (token == address(eeth)) {
+            eeth.mint(msg.sender, amount);
+        } else {
+            revert("Invalid token");
+        }
     }
-
 }
 
-contract DeployOFTScript is Script, L2Constants {
+contract deployMainnetMock is Script, L2Constants {
 
     address constant sepoliaGnosis = 0x05b0f5a18AA3705dFf391f87c4BdD69eA6b8f80B;
 
@@ -51,6 +59,9 @@ contract DeployOFTScript is Script, L2Constants {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
 
+        mockEETH eeth = new mockEETH();
+        mockWEETH weeth = new mockWEETH(address(eeth));
+        
         address[] memory controllers = new address[](1);
         controllers[0] = sepoliaGnosis;
         new EtherFiTimelock(1, new address[](0), new address[](0), sepoliaGnosis);
