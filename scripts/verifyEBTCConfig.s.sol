c50bef1e9b20d20290976f0ecf98a97de07a7dba
jtfirek <firekjt@gmail.com>
2024-12-20 11:44:49 -0500

    fixing script

--- scripts/verifyEBTCConfig.s.sol
+++ scripts/verifyEBTCConfig.s.sol
@@ -28,6 +28,12 @@ struct ConfigPerChain {
     address RECEIVE_302;
     address LAYERZERO_DVN;
     address NETHERMIND_DVN;
+    address GNOSIS_SAFE;
+}
+
+// allows us to interface with the delegates map that isn't defined in the ILayerZeroEndpointV2 inteface
+interface EndpointDelegates {
+    function delegates(address) external view returns (address);
 }
 
 contract eBTCVerifier is Script, LayerZeroHelpers, Test {
@@ -56,21 +62,27 @@ contract eBTCVerifier is Script, LayerZeroHelpers, Test {
         ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(chain.ENDPOINT);
 
         console.log("Starting verifications of configurations on:", chain.NAME);
-        console.log("====================================");   
+        console.log("====================================");
+
+        
 
         for (uint i = 0; i < chains.length; i++) {
 
+            address delegate = EndpointDelegates(chain.ENDPOINT).delegates(LAYER_ZERO_TELLER);
+            assertEq(delegate, chain.GNOSIS_SAFE, "Delegate not set correctly");
 
             ConfigPerChain memory peerChain = chains[i];
             if (keccak256(abi.encodePacked(chain.NAME)) == keccak256(abi.encodePacked(peerChain.NAME))) {
                 continue;
             }
-            console.log("Verifying configurations for peer:", peerChain.NAME);
+            console.log("Verifying LZ configurations for peer:", peerChain.NAME);
             
             assertEq(endpoint.getConfig(LAYER_ZERO_TELLER, chain.SEND_302, peerChain.EID, 2), _getExpectedUln(chain.LAYERZERO_DVN, chain.NETHERMIND_DVN), "Send config not set correctly");
             assertEq(endpoint.getConfig(LAYER_ZERO_TELLER, chain.RECEIVE_302, peerChain.EID, 2), _getExpectedUln(chain.LAYERZERO_DVN, chain.NETHERMIND_DVN), "Receive config not set correctly");
 
+            // verify peer on both the LayerZeroTeller and the endpoint
             bytes32 peerContract = LayerZeroTeller.peers(peerChain.EID);
+            assertEq(peerContract, _toBytes32(LAYER_ZERO_TELLER), "Peer contract not set correctly");
             assertEq(peerContract, _toBytes32(LAYER_ZERO_TELLER), "Peer contract not set correctly");
 
             (,,uint256 limit, uint256 window) = LayerZeroTeller.outboundRateLimits(peerChain.EID);
@@ -81,6 +93,8 @@ contract eBTCVerifier is Script, LayerZeroHelpers, Test {
             assertEq(limit, LIMIT, "Limit not set correctly peer");
             assertEq(window, WINDOW, "Window not set correctly peer");
         }
+
+        console.log("All LZ configurations verified for:", chain.NAME);
 
         console.log("====================================\n");   
     }
@@ -105,7 +119,8 @@ contract eBTCVerifier is Script, LayerZeroHelpers, Test {
         RECEIVE_302: 0x2367325334447C5E1E0f1b3a6fB947b262F58312,
 
         LAYERZERO_DVN: 0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842,
-        NETHERMIND_DVN: 0xE33de1A8cf9bcdC6b509C44EEF66f47c65dA6d47
+        NETHERMIND_DVN: 0xE33de1A8cf9bcdC6b509C44EEF66f47c65dA6d47, 
+        GNOSIS_SAFE: 0x3FF1db55460125C667f5d2c68aAaBdeDf3FB2948
     });
 
     ConfigPerChain BASE = ConfigPerChain({
@@ -118,7 +133,8 @@ contract eBTCVerifier is Script, LayerZeroHelpers, Test {
         RECEIVE_302: 0xc70AB6f32772f59fBfc23889Caf4Ba3376C84bAf,
 
         LAYERZERO_DVN: 0x9e059a54699a285714207b43B055483E78FAac25, 
-        NETHERMIND_DVN: 0xcd37CA043f8479064e10635020c65FfC005d36f6
+        NETHERMIND_DVN: 0xcd37CA043f8479064e10635020c65FfC005d36f6,
+        GNOSIS_SAFE: address(0)
     });
 
     ConfigPerChain ETH = ConfigPerChain({
@@ -131,7 +147,8 @@ contract eBTCVerifier is Script, LayerZeroHelpers, Test {
         RECEIVE_302: 0xc02Ab410f0734EFa3F14628780e6e695156024C2,
 
         LAYERZERO_DVN: 0x589dEDbD617e0CBcB916A9223F4d1300c294236b, 
-        NETHERMIND_DVN: 0xa59BA433ac34D2927232918Ef5B2eaAfcF130BA5
+        NETHERMIND_DVN: 0xa59BA433ac34D2927232918Ef5B2eaAfcF130BA5,
+        GNOSIS_SAFE: address(0)
     });
 
     ConfigPerChain ARB = ConfigPerChain({
@@ -144,6 +161,7 @@ contract eBTCVerifier is Script, LayerZeroHelpers, Test {
         RECEIVE_302: 0x7B9E184e07a6EE1aC23eAe0fe8D6Be2f663f05e6,
 
         LAYERZERO_DVN: 0x2f55C492897526677C5B68fb199ea31E2c126416, 
-        NETHERMIND_DVN: 0xa7b5189bcA84Cd304D8553977c7C614329750d99
+        NETHERMIND_DVN: 0xa7b5189bcA84Cd304D8553977c7C614329750d99,
+        GNOSIS_SAFE: address(0)
     });
 }
