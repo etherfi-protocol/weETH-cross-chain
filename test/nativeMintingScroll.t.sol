4d9b1cc02f6437383b8dca205641e5a2ec1a19d9
jtfirek <firekjt@gmail.com>
2025-01-08 14:14:47 -0500

    setting all files to .20

--- test/nativeMintingScroll.t.sol
+++ test/nativeMintingScroll.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.20;
 
 import "../scripts/NativeMintingDeployment/DeployConfigureL1.s.sol";
 import "../scripts/NativeMintingDeployment/DeployConfigureL2.s.sol";
