0bbdbe5fcc41a9d79c827bdf4a42f501f1e6f758
jtfirek <firekjt@gmail.com>
2025-09-15 13:48:31 -0400

    plasma deployment

--- scripts/oft-deployment/04_OFTVerification.s.sol
+++ scripts/oft-deployment/04_OFTVerification.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {console2} from "forge-std/console2.sol";
 import "forge-std/Test.sol";
-
+import "../../contracts/EtherFiTimelock.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "../../contracts/EtherfiOFTUpgradeable.sol";
 import "../../utils/L2Constants.sol";
@@ -38,16 +38,30 @@ contract verifyOFT is ContractCodeChecker, Script, L2Constants, Test {
         EtherfiOFTUpgradeable oft = EtherfiOFTUpgradeable(DEPLOYMENT_OFT);
         LZEndpoint endpoint = LZEndpoint(DEPLOYMENT_LZ_ENDPOINT);
 
-        assertEq(ProxyAdmin(DEPLOYMENT_PROXY_ADMIN_CONTRACT).owner(), DEPLOYMENT_CONTRACT_CONTROLLER);
+        assertEq(ProxyAdmin(DEPLOYMENT_PROXY_ADMIN_CONTRACT).owner(), L2_TIMELOCK);
 
-        assertEq(oft.owner(), DEPLOYMENT_CONTRACT_CONTROLLER);
+        assertEq(oft.owner(), L2_TIMELOCK);
         assertTrue(oft.hasRole(oft.PAUSER_ROLE(), PAUSER_EOA));
         assertTrue(oft.hasRole(oft.UNPAUSER_ROLE(), DEPLOYMENT_CONTRACT_CONTROLLER));
-        assertTrue(oft.hasRole(oft.DEFAULT_ADMIN_ROLE(), DEPLOYMENT_CONTRACT_CONTROLLER));
+        assertTrue(oft.hasRole(oft.DEFAULT_ADMIN_ROLE(), L2_TIMELOCK));
         assertFalse(oft.hasRole(oft.DEFAULT_ADMIN_ROLE(), DEPLOYER_ADDRESS));
 
         assertEq(endpoint.delegates(DEPLOYMENT_OFT), DEPLOYMENT_CONTRACT_CONTROLLER);
-    
+
+        console2.log("All roles are correct!\n");
+
+        console2.log("#4. verify timelock bytecode and roles..\n");
+        address[] memory controller = new address[](1);
+        controller[0] = DEPLOYMENT_CONTRACT_CONTROLLER;
+        EtherFiTimelock timelockTest = new EtherFiTimelock(3 days, controller, controller, DEPLOYMENT_OFT);
+        verifyContractByteCodeMatch(L2_TIMELOCK, address(timelockTest));
+
+        EtherFiTimelock timelock = EtherFiTimelock(payable(L2_TIMELOCK));
+        assertEq(timelock.hasRole(timelock.DEFAULT_ADMIN_ROLE(), L2_TIMELOCK), true);
+        assertEq(timelock.hasRole(timelock.PROPOSER_ROLE(), DEPLOYMENT_CONTRACT_CONTROLLER), true);
+        assertEq(timelock.hasRole(timelock.EXECUTOR_ROLE(), DEPLOYMENT_CONTRACT_CONTROLLER), true);
+        assertEq(timelock.hasRole(timelock.CANCELLER_ROLE(), DEPLOYMENT_CONTRACT_CONTROLLER), true);
+
         console2.log("All roles are correct!\n");
     }
 }
