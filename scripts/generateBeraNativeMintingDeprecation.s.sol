c24d11e9fdc9e0c64ec0b6a0704a0fa3458b983d
jtfirek <firekjt@gmail.com>
2025-10-04 20:16:56 -0400

    change to bera

--- scripts/generateBeraNativeMintingDeprecation.s.sol
+++ scripts/generateBeraNativeMintingDeprecation.s.sol
@@ -5,13 +5,14 @@ import "forge-std/Script.sol";
 import "forge-std/Test.sol";
 import "../utils/L2Constants.sol";
 import "../utils/GnosisHelpers.sol";
-import "../contracts/native-minting/l2-syncpools/L2OPStackSyncPoolETHUpgradeable.sol";
+import "../contracts/native-minting/l2-syncpools/HydraSyncPoolETHUpgradeable.sol";
 import "../contracts/native-minting/layerzero-base/L2BaseSyncPoolUpgradeable.sol";
 import "../contracts/EtherfiOFTUpgradeable.sol";
 
-contract GenerateLineaNativeMintingDeprecation is Script, Test, L2Constants, GnosisHelpers {
+contract GenerateBeraNativeMintingDeprecation is Script, Test, L2Constants, GnosisHelpers {
     
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    address constant HYDRA_WETH = 0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590;
 
     function run() public {
         _generateTransactionBundle();
@@ -22,21 +23,21 @@ contract GenerateLineaNativeMintingDeprecation is Script, Test, L2Constants, Gno
         bytes memory revokeRoleData = abi.encodeWithSignature(
             "revokeRole(bytes32,address)", 
             MINTER_ROLE, 
-            LINEA.L2_SYNC_POOL
+            BERA.L2_SYNC_POOL
         );
         
         bytes memory setMinSyncData = abi.encodeWithSignature(
             "setMinSyncAmount(address,uint256)", 
-            ETH_ADDRESS, 
+            HYDRA_WETH, 
             0
         );
         
-        string memory transactionJson = _getGnosisHeader(LINEA.CHAIN_ID, LINEA.L2_CONTRACT_CONTROLLER_SAFE);
+        string memory transactionJson = _getGnosisHeader(BERA.CHAIN_ID, BERA.L2_CONTRACT_CONTROLLER_SAFE);
         
         transactionJson = string.concat(
             transactionJson, 
             _getGnosisTransaction(
-                addressToHex(LINEA.L2_OFT), 
+                addressToHex(BERA.L2_OFT), 
                 iToHex(revokeRoleData), 
                 false
             )
@@ -45,27 +46,27 @@ contract GenerateLineaNativeMintingDeprecation is Script, Test, L2Constants, Gno
         transactionJson = string.concat(
             transactionJson, 
             _getGnosisTransaction(
-                addressToHex(LINEA.L2_SYNC_POOL), 
+                addressToHex(BERA.L2_SYNC_POOL), 
                 iToHex(setMinSyncData), 
                 true
             )
         );
         
-        vm.writeJson(transactionJson, "./output/linea-native-minting-deprecation-bundle.json");
+        vm.writeJson(transactionJson, "./output/bera-native-minting-deprecation-bundle.json");
     }
 
     function _testTransactionsOnFork() internal {
-        vm.createSelectFork(LINEA.RPC_URL);
+        vm.createSelectFork(BERA.RPC_URL);
         
-        executeGnosisTransactionBundle("./output/linea-native-minting-deprecation-bundle.json", LINEA.L2_CONTRACT_CONTROLLER_SAFE);
+        executeGnosisTransactionBundle("./output/bera-native-minting-deprecation-bundle.json", BERA.L2_CONTRACT_CONTROLLER_SAFE);
         
-        EtherfiOFTUpgradeable oft = EtherfiOFTUpgradeable(LINEA.L2_OFT);
-        L2OPStackSyncPoolETHUpgradeable syncPool = L2OPStackSyncPoolETHUpgradeable(LINEA.L2_SYNC_POOL);
+        EtherfiOFTUpgradeable oft = EtherfiOFTUpgradeable(BERA.L2_OFT);
+        HydraSyncPoolETHUpgradeable syncPool = HydraSyncPoolETHUpgradeable(BERA.L2_SYNC_POOL);
         
-        bool hasMinterRoleAfter = oft.hasRole(MINTER_ROLE, LINEA.L2_SYNC_POOL);
+        bool hasMinterRoleAfter = oft.hasRole(MINTER_ROLE, BERA.L2_SYNC_POOL);
         assertFalse(hasMinterRoleAfter, "MINTER_ROLE should be revoked");
         
-        L2BaseSyncPoolUpgradeable.Token memory tokenData = syncPool.getTokenData(ETH_ADDRESS);
-        assertEq(tokenData.minSyncAmount, 0, "Min sync amount should be set to 0 for ETH");
+        L2BaseSyncPoolUpgradeable.Token memory tokenData = syncPool.getTokenData(HYDRA_WETH);
+        assertEq(tokenData.minSyncAmount, 0, "Min sync amount should be set to 0 for WETH");
     }
 }
