aff2273d55558e7a5bfad9a271bf9cd74b5127d3
Yash Saraswat <yash@ether.fi>
2026-06-29 09:38:49 -0500

    chore: update deploy commit hash for l1 sync pool

--- scripts/security-upgrade/DeployL1SyncPoolImpl.s.sol
+++ scripts/security-upgrade/DeployL1SyncPoolImpl.s.sol
@@ -17,7 +17,7 @@ contract DeployL1SyncPoolImpl is Script, L2Constants {
         vm.startBroadcast(DEPLOYER_ADDRESS);
 
         address impl = address(
-            new EtherfiL1SyncPoolETH{salt: bytes32(bytes20(hex"3a90e505b140accd42c6d3e220a18289ec693c6f"))}(L1_ENDPOINT, ROLE_REGISTRY)
+            new EtherfiL1SyncPoolETH{salt: bytes32(bytes20(hex"c6997994314a62143a923fdd61c9a11b068d8092"))}(L1_ENDPOINT, ROLE_REGISTRY)
         );
 
         console.log("EtherfiL1SyncPoolETH implementation deployed at:", impl);
